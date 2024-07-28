3f0923cb34690e4a44cbdb1b56ad616e4228cde9
dale-cale9 <145309461+dale-cale9@users.noreply.github.com>
2024-07-29 09:41:01 +1200

    .
    idk what im doing

--- code/5-sale-calculator.js
+++ code/5-sale-calculator.js
@@ -3,11 +3,21 @@
 // ####################################
 
 function saleCalculator() {
-  let amount = prompt ('How much money do you have?')
-  let price = prompt ('What is the price of the item?')
-  let discount = prompt ('What is the discount percentage?')
+  let amount = prompt('How much money do you have?')
+  let price = prompt('What is the price of the item?')
+  let discount = prompt('What is the discount percentage?')
 
+  answer = price / 100 * discount
+  finalePrice = price - answer
 
+  if (finalePrice > amount) {
+    alert('You do not have enough money to buy that product.')
+  }
+  else if (finalePrice <= amount) {
+    alert('YAYYY you have enough money to buy that product.')
+  } else {
+    alert ('error')
+  }
 
 
 }
